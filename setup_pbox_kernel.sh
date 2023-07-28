f71fded7d39503f5b629a9bd640f2ba9564585fe
gongxini <hyigong1@jhu.edu>
2023-07-28 09:21:12 -0600

    update compile file

--- setup_pbox_kernel.sh
+++ setup_pbox_kernel.sh
@@ -6,8 +6,7 @@ pip3 install pandas matplotlib
 cp .config src/
 cd src && make kvm_guest.config
 cd src && make -j 20 bindeb-pkg LOCALVERSION=-my-k
+cd ..
 sudo dpkg -i linux-image-5.4.0-my-k_5.4.0-my-k-1_amd64.deb \
 linux-headers-5.4.0-my-k_5.4.0-my-k-1_amd64.deb \
 linux-libc-dev_5.4.0-my-k-1_amd64.deb 
-sudo reboot
-
